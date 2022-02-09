59629d14d927ca78b897e7969e6a2422a627a6a9
tipachel88 <tipakoroche@gmail.com>
2022-02-09 21:22:29 +0300

    Initial Commit

---
+++ ContactList/ContactListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListApp.swift
+//  ContactList
+//
+//  Created by Tipachel on 09.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
